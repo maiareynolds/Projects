fcf733a456843da0f0a0db68648cf4d41ea66618
maiareynolds <35492770+maiareynolds@users.noreply.github.com>
2018-04-13 09:16:47 -0400

    Updated from Brython Server: 4/13/2018 9:16:46 AM

--- graphicsHangman.py
+++ graphicsHangman.py
@@ -4,53 +4,37 @@
 
 from ggame import *
 from random import randint
-"""
+
 def pickWord(): #picks a "random" word from a list of words
     i=randint(1,5)
     if i==1:
-        word="Awkward"
+        data["word"]="awkward"
+        data["number"]=7
     elif i==2:
-        word="Croquet"
+        data["word"]="croquet"
+        data["number"]=7
     elif i==3:
-        word="Fjord"
+        data["word"]="fjord"
+        data["number"]=5
     elif i==4:
-        word="Kiosk"
+        data["word"]="kiosk"
+        data["number"]=5
     else:
-        word="Quip"
+        data["word"]="quip"
+        data["number"]=4
 
 def wordComplete(): #returns True if all letters in word have been guessed, false otherwise
-    Sprite(RectangleAsset(1200,800,LineStyle(3,Color(0xBBFFFF,1)),Color(0xBBFFFF,1)))
-    Sprite(RectangleAsset(1200,400,LineStyle(3,Color(0x00FF7F,1)),Color(0x00FF7F,1)),(0,300))
-    Sprite(TextAsset("True, the word is",fill=Color(0xFF1493,1),style="40pt Times bold"),(150,150)))#work on
-    Sprite(TextAsset(word,fill=Color(0xFF1493,1),style="40pt Times bold",(250,200)))
-
-    if str(data["letter"]) not in str(data["word"]):
-"""
-def keyPress(event): #puts letter in word if correct and  puts letter in list of guessed letters
-    letter=event.key
-    data["letter"]=letter
-    print(data["letter"])#take out
-    x1=0
-    if letter not in word:
-        data["incorrect"]+=1
-        printHangman(0)
-    else:
-        for ch in word:
-            if letter==ch:
-                Sprite(TextAsset(letter,fill=Color(0x000000,1),style="15pt Times"),(135+x1,325))
-            x1+=30
-        x1+=30
-    Sprite(TextAsset(letter,fill=Color(0x000000,1),style="15pt Times"),(x1,0))#FIX
-"""
-        data["correct"]+=1
-        if data["correct"]==:
-            wordComplete()
-"""
+    if data["incorrect"]==7:#prints false if hangman is complete
+        Sprite(RectangleAsset(1100,600,LineStyle(4,Color(0x000000,1)),Color(0x000000,1)))
+        Sprite(TextAsset("False",fill=Color(0xFF0000,1),style="60pt Times bold"),(420,200))
+        Sprite(TextAsset("Game"+"Over",fill=Color(0xFF0000,1),style="60pt Times bold"),(325,260))
+    if data["correct"]==data["number"]:#prints true if you win
+        Sprite(RectangleAsset(1200,1000,LineStyle(3,Color(0xBBFFFF,1)),Color(0xBBFFFF,1)))
+        Sprite(TextAsset("True",fill=Color(0xFF1493,1),style="40pt Times"),(450,150))
 
 
 def printHangman(incorrect): #prints out new body part with each wrong guess
-    incorrect=data["incorrect"]
-    while incorrect<=6:
+    if incorrect<=6:
         if incorrect==1:
             Sprite(CircleAsset(15,LineStyle(1,Color(0xFF1493,1)),Color(0xFF1493,1)),(292.5,95))#head
         elif incorrect==2:
@@ -63,9 +47,29 @@ def printHangman(incorrect): #prints out new body part with each wrong guess
             Sprite(LineAsset(-30,30,LineStyle(4,Color(0xFF1493,1))),(274,190))#leg1
         else:
             Sprite(LineAsset(30,30,LineStyle(4,Color(0xFF1493,1))),(303,190))#leg2
-    Sprite(RectangleAsset(1100,600,LineStyle(4,Color(0x000000,1)),Color(0x000000,1)))
-    Sprite(TextAsset("False",fill=Color(0xFF0000,1),style="60pt Times bold"),(420,200))
-    Sprite(TextAsset("Game"+"Over",fill=Color(0xFF0000,1),style="60pt Times bold"),(325,260))
+    else:
+       wordComplete()#once the hangman is complete
+
+
+
+def keyPress(event): #puts letter in word if correct and  puts letter in list of guessed letters
+    data["letter"]=event.key
+    x1=0
+    if data["letter"] not in data["word"]:
+        data["incorrect"]+=1
+        printHangman(data["incorrect"])
+    else:
+        for ch in data["word"]:#if it it correct, fills in blank
+            if data["letter"]==ch:
+                Sprite(TextAsset(data["letter"],fill=Color(0x000000,1),style="15pt Times"),(135+x1,325))
+            x1+=30
+        x1+=30
+        if data["letter"] in data["word"]:
+            data["correct"]+=1
+            if data["correct"]==data["number"]:
+                wordComplete()
+    Sprite(TextAsset(data["letter"],fill=Color(0x000000,1),style="15pt Times"),(x1,0))#FIX
+
 
 if __name__=="__main__":
     data={}
@@ -73,8 +77,9 @@ if __name__=="__main__":
     data["incorrect"]=0
     data["correct"]=0
     data["letter"]=0
-    word="watermelon"
-    data["word"]=word
+    data["word"]=0
+    data["number"]=0
+    pickWord()
     #Background Graphics
     Sprite(RectangleAsset(1200,800,LineStyle(3,Color(0x9FB6CD,1)),Color(0x9FB6CD,1)))#sky
     Sprite(RectangleAsset(1200,400,LineStyle(3,Color(0x8B4726,1)),Color(0x8B4726,1)),(0,300))#ground
@@ -87,19 +92,10 @@ if __name__=="__main__":
     Sprite(TextAsset("Hangman",fill=Color(0x00CED1,1),style="15pt Times"),(930,500))#logo
     #Letter Line Graphics
     x=0
-    for ch in word:
+    for ch in data["word"]:
         Sprite(LineAsset(20,0,LineStyle(3,Color(0x000000,1))),(125+x,350))
         x+=30
 
-"""
-#Man Graphics
-Sprite(CircleAsset(15,LineStyle(1,Color(0xFF1493,1)),Color(0xFF1493,1)),(292.5,95))#head
-Sprite(LineAsset(0,70,LineStyle(4,Color(0xFF1493,1))),(303,120))#body
-Sprite(LineAsset(30,30,LineStyle(4,Color(0xFF1493,1))),(274,125))#arm1
-Sprite(LineAsset(-30,30,LineStyle(4,Color(0xFF1493,1))),(303,125))#arm2
-Sprite(LineAsset(-30,30,LineStyle(4,Color(0xFF1493,1))),(274,190))#leg1
-Sprite(LineAsset(30,30,LineStyle(4,Color(0xFF1493,1))),(303,190))#leg2
-"""
 App().listenKeyEvent("keydown","a",keyPress)
 App().listenKeyEvent("keydown","b",keyPress)
 App().listenKeyEvent("keydown","c",keyPress)
@@ -128,16 +124,3 @@ App().listenKeyEvent("keydown","y",keyPress)
 App().listenKeyEvent("keydown","z",keyPress)
 
 App().run()
-
-"""
-#consider adding difficulty levels
-
--------------------------------------------------------
-Useful detail - event.key contains the key that was pressed to trigger the key press function. You will need to listen for 26 different keys (use a loop to shorten this code!)
-
--------------------------------------------------------
-Ideas for extensions:
-1) Make your graphics really cool (animation?)
-2) Come up with a scoring system
-3) Make different levels (easy, medium, hard)
-4) Research which words are the hardest to guess"""
