a873955aef4dd8fea612a8c4dc65c4a74909fe48
maiareynolds <35492770+maiareynolds@users.noreply.github.com>
2018-05-23 20:05:28 -0400

    Updated from Brython Server: 5/23/2018 8:05:27 PM

--- battleshippractice.py
+++ battleshippractice.py
@@ -5,8 +5,7 @@
 from ggame import *
 from random import randint
 
-#makes 2 10x10 matrices and returns them
-def buildBoard():
+def buildBoard():#makes 2 10x10 matrices and returns them
     rows=0
     columns=0
     while columns<400:
@@ -22,13 +21,6 @@ def buildBoard():
             rows+=40
         columns+=40
         rows=0
-    Sprite(TextAsset("Computer",fill=Color(0xFF3030,1),style="30pt Georgia bold"),(725,400))
-    Sprite(TextAsset("You",fill=Color(0xFF3030,1),style="30pt Georgia bold"),(170,400))
-    Sprite(TextAsset("Ships",fill=Color(0xFF3030,1),style="30pt Georgia bold"),(455,0))
-    Sprite(RectangleAsset(80,40,LineStyle(3,Color(0x104E8B,1)),Color(0xFF3E96,1)),(450,50))
-    Sprite(TextAsset("x3",fill=Color(0xFF3030,1),style="30pt Georgia bold"),(535,50))
-    Sprite(RectangleAsset(120,40,LineStyle(3,Color(0x104E8B,1)),Color(0xFF3E96,1)),(410,100))
-    Sprite(TextAsset("x2",fill=Color(0xFF3030,1),style="30pt Georgia bold"),(535,100))
     return
 
 #Deletes all graphics on the board and draws the player board and computer board in their current state
@@ -39,7 +31,12 @@ def redrawAll():
 #Has the computer pick 5 random spaces for their ships.
 #The computer should not be able to put two ships on top of each other
 def pickComputerShips():
-    ...
+    x=randint(0,8)
+    y=randint(0,8)
+    if data["computerY"][data["computerX"].index(x)]
+    
+    data["computerX"].append(600+x*40)
+    data["computerY"].append(y*40)
 
 #The function should have the computer pick a random spot to guess and process the guess if it is a valid move.
 #This function should also detect if the computer won.
@@ -54,19 +51,27 @@ def mouseClick(event):
         y=(event.y-event.y%40)
         Sprite(RectangleAsset(40,40,LineStyle(3,Color(0x104E8B,1)),Color(0xFF3E96,1)),(x,y))
     if event.x>600 and event.y<400:
-        if (event.x-event.x%40) in data["xvalues"] and (event.y-event.y%40) in data["yvalues"]:
+        if data["computerX"].index((event.x-event.x%40))==data["computerY"].index((event.y-event.y%40)):
             Sprite(RectangleAsset(40,40,LineStyle(3,Color(0x104E8B,1)),Color(0xFF3E96,1)),(x,y))
         else:
             Sprite(RectangleAsset(40,40,LineStyle(3,Color(0x104E8B,1)),Color(0x00F5FF,1)),(x,y))
 
 if __name__ == '__main__':
+    Sprite(TextAsset("Computer",fill=Color(0xFF3030,1),style="30pt Georgia bold"),(725,400))
+    Sprite(TextAsset("You",fill=Color(0xFF3030,1),style="30pt Georgia bold"),(170,400))
+    Sprite(TextAsset("Ships",fill=Color(0xFF3030,1),style="30pt Georgia bold"),(455,0))
+    Sprite(RectangleAsset(80,40,LineStyle(3,Color(0x104E8B,1)),Color(0xFF3E96,1)),(450,50))
+    Sprite(TextAsset("x3",fill=Color(0xFF3030,1),style="30pt Georgia bold"),(535,50))
+    Sprite(RectangleAsset(120,40,LineStyle(3,Color(0x104E8B,1)),Color(0xFF3E96,1)),(410,100))
+    Sprite(TextAsset("x2",fill=Color(0xFF3030,1),style="30pt Georgia bold"),(535,100))
     buildBoard()
     data={}
-    data["xvalues"]=[]
-    data["yvalues"]=[]
+    data["computerX"]=[]
+    data["computerY"]=[]
 
 App().run()
 App().listenMouseEvent("click",mouseClick)
+
 #look at matrix demo
 #To delete all graphics, you can use a for loop.
 #for item in App().spritelist[:]:
