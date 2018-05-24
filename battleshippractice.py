b7eadd3b1c188d49c7f69dcb5e2b8cdf5fc2bd6d
maiareynolds <35492770+maiareynolds@users.noreply.github.com>
2018-05-24 08:31:03 -0400

    Updated from Brython Server: 5/24/2018 8:31:03 AM

--- battleshippractice.py
+++ battleshippractice.py
@@ -30,14 +30,11 @@ def redrawAll():
 
 #Has the computer pick 5 random spaces for their ships.
 #The computer should not be able to put two ships on top of each other
-"""def pickComputerShips():
-    x=randint(0,8)
-    y=randint(0,8)
-    if data["computerY"][data["computerX"].index(x)]
-    
-    data["computerX"].append(600+x*40)
-    data["computerY"].append(y*40)
-"""
+def pickComputerShips():
+    computerx=[]
+    computery=[]
+    computerx.append(randint(0,8))
+
 #The function should have the computer pick a random spot to guess and process the guess if it is a valid move.
 #This function should also detect if the computer won.
 def computerTurn():
@@ -46,20 +43,45 @@ def computerTurn():
 #what row and column the user clicked (event.x and .y have the coordinates)
 #if player hasnt placed ships, place ship, if has placed ship, process user's guess if valid and detect if player won
 def mouseClick(event):
+    data["click"]+=1
     if event.x<400 and event.y<400:
-        x=(event.x-event.x%40)
-        y=(event.y-event.y%40)
-        Sprite(RectangleAsset(40,40,LineStyle(3,Color(0x104E8B,1)),Color(0xFF3E96,1)),(x,y))
+        if data["click"]==1 or data["click"]==3 or data["click"]==5:
+            data["x1"]=(event.x-event.x%40)
+            data["y1"]=(event.y-event.y%40)
+            option1=Sprite(RectangleAsset(120,40,LineStyle(3,Color(0x104E8B,1)),Color(0xEE82EE,1)),(data["x1"]-40,data["y1"]))
+            option2=Sprite(RectangleAsset(40,120,LineStyle(3,Color(0x104E8B,1)),Color(0xEE82EE,1)),(data["x1"],data["y1"]-40))
+            Sprite(RectangleAsset(40,40,LineStyle(3,Color(0x104E8B,1)),Color(0xFF3E96,1)),(data["x1"],data["y1"]))
+        if data["click"]==2 or data["click"]==4 or data["click"]==6:
+            option1.destroy()
+            option2.destroy()
+            data["x2"]=(event.x-event.x%40)
+            data["y2"]=(event.y-event.y%40)
+            if data["x2"]==data["x1"]-40:
+                Sprite(RectangleAsset(80,40,LineStyle(3,Color(0x104E8B,1)),Color(0xFF3E96,1)),(data["x2"],data["y2"]))
+            elif data["x2"]==data["x1"]+40:
+                Sprite(RectangleAsset(80,40,LineStyle(3,Color(0x104E8B,1)),Color(0xFF3E96,1)),(data["x1"],data["y1"]))
+            elif data["y2"]==data["y1"]-40:
+                Sprite(RectangleAsset(40,80,LineStyle(3,Color(0x104E8B,1)),Color(0xFF3E96,1)),(data["x2"],data["y2"]))
+            elif data["y2"]==data["y1"]+40:
+                Sprite(RectangleAsset(40,80,LineStyle(3,Color(0x104E8B,1)),Color(0xFF3E96,1)),(data["x1"],data["y1"]))
+            else:
+                data["click"]-=1
     elif event.x>600 and event.y<400:
         x=(event.x-event.x%40)
         y=(event.y-event.y%40)
         Sprite(RectangleAsset(40,40,LineStyle(3,Color(0x104E8B,1)),Color(0xFF3E96,1)),(x,y))
-"""        if data["computerX"].index((event.x-event.x%40))==data["computerY"].index((event.y-event.y%40)):
-            Sprite(RectangleAsset(40,40,LineStyle(3,Color(0x104E8B,1)),Color(0xFF3E96,1)),(x,y))
-        else:
-            Sprite(RectangleAsset(40,40,LineStyle(3,Color(0x104E8B,1)),Color(0x00F5FF,1)),(x,y))"""
+
+#other color: 00F5FF
 
 if __name__ == '__main__':
+    data={}
+    data["click"]=0
+    data["x1"]=0
+    data["y1"]=0
+    data["x2"]=0
+    data["y2"]=0
+    print("To place your ships, click the box you want the ship to start in and the box you want it to end in")
+    print("start with the two space ships and move to the three space ships")
     Sprite(TextAsset("Computer",fill=Color(0xFF3030,1),style="30pt Georgia bold"),(725,400))
     Sprite(TextAsset("You",fill=Color(0xFF3030,1),style="30pt Georgia bold"),(170,400))
     Sprite(TextAsset("Ships",fill=Color(0xFF3030,1),style="30pt Georgia bold"),(455,0))
@@ -68,9 +90,6 @@ if __name__ == '__main__':
     Sprite(RectangleAsset(120,40,LineStyle(3,Color(0x104E8B,1)),Color(0xFF3E96,1)),(410,100))
     Sprite(TextAsset("x2",fill=Color(0xFF3030,1),style="30pt Georgia bold"),(535,100))
     buildBoard()
-    data={}
-    data["computerX"]=[]
-    data["computerY"]=[]
 
 App().run()
 App().listenMouseEvent("click",mouseClick)
