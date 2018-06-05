01acd262a16e9a10b01848cbaef6ea42a931b2bb
maiareynolds <35492770+maiareynolds@users.noreply.github.com>
2018-06-04 21:47:19 -0400

    Updated from Brython Server: 6/4/2018 9:47:19 PM

--- calculus.py
+++ calculus.py
@@ -3,24 +3,15 @@ from math import *
 
 
 
-list1=["x**2","e**(x/2)","e**(x*(2/3))","x**3"]
+list1=["e**(x/2)","e**(x)","(log(x)/log(e))**x","x**x"]
 num=[]
 
-x=100000000000000000000000
-functions=[x**2,e**(x/2),e**(x*(2/3)),x**3]
-functions1=[x**2,e**(x/2),e**(x*(2/3)),x**3]
+x=100000
+functions=[e**(x/2),e**(x),(log(x)/log(e))**x,x**x]
+functions1=[e**(x/2),e**(x),(log(x)/log(e))**x,x**x]
 l=len(functions)
 newlist=[]
 newlist1=[]
-
-"""
-while l!=len(newlist):
-    for item in functions:
-        if item==max(functions):
-            newlist.append(item)
-            num.append(functions.index(item))
-            functions.remove(item)
-"""
 
 while l>len(newlist):
     for item in functions:
