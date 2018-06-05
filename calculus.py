71e6494b427a2f0d19c5c83078dae82b0ebbe530
maiareynolds <35492770+maiareynolds@users.noreply.github.com>
2018-06-04 20:50:22 -0400

    Updated from Brython Server: 6/4/2018 8:50:22 PM

--- calculus.py
+++ calculus.py
@@ -1,12 +1,28 @@
 from math import *
 
-x=10000
+list1=["e**(x/2)","e**(x)","(log(x)/log(e))**x","x**x"]
+num=[]
+
+x=100
 functions=[e**(x/2),e**(x),(log(x)/log(e))**x,x**x]
 l=len(functions)
 newlist=[]
 
+"""
 while l!=len(newlist):
     for item in functions:
         if item==max(functions):
             newlist.append(item)
+            num.append(functions.index(item))
             functions.remove(item)
+"""
+
+while l!=len(newlist):
+    for item in functions:
+        if item==max(functions):
+            num.append(functions.index(item))
+            functions.remove(item)
+    for item in num:
+        newlist.append(list1[item])
+
+print(newlist)
