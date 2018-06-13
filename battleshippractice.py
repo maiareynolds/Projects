36db80b3d11bcc40ba1cb63460d82e38690a4ddd
maiareynolds <35492770+maiareynolds@users.noreply.github.com>
2018-06-13 08:02:16 -0400

    Updated from Brython Server: 6/13/2018 8:02:15 AM

--- battleshippractice.py
+++ battleshippractice.py
@@ -23,13 +23,7 @@ def buildBoard():#makes 2 10x10 matrices and returns them
         rows=0
     return
 
-#Deletes all graphics on the board and draws the player board and computer board in their current state
-#allow for ships that are more than one space
-def redrawAll():
-    ...
-
 #Has the computer pick 5 random spaces for their ships.
-#The computer should not be able to put two ships on top of each other
 def pickComputerShips():
     i=0
     xl=[]
@@ -38,7 +32,7 @@ def pickComputerShips():
         x=randint(0,4)
         y=randint(0,4)
         if len(xl)>0:
-            if x in xl and y==yl[xl.index(x)]:
+            if x in xl and y==yl[xl.index(x)]:#makes sure computer can't put ships in the same spot
                 while x in xl and y==yl[xl.index(x)]:
                     x=randint(1,4)
                     y=randint(1,4)
@@ -46,12 +40,11 @@ def pickComputerShips():
         yl.append(y)
         i+=1
     for item in xl:
-        data["compShipsx"].append(item*80)
+        data["compShipsx"].append(item*80)#makes coordinates accessible to other functions
         data["compShipsy"].append(yl[xl.index(item)]*80)
 
 
-#The function should have the computer pick a random spot to guess and process the guess if it is a valid move.
-#This function should also detect if the computer won.
+#Computer picka a random spot to guess and process the guess if it is a valid move, detects if the computer won.
 def computerTurn():
     x=randint(0,4)
     y=randint(0,4)
@@ -59,14 +52,14 @@ def computerTurn():
         while x*80 in data["xmoves"] and y*80==data["ymoves"][data["xmoves"].index(x*80)]:
             x=randint(0,4)
             y=randint(0,4)
-    data["ymoves"].append(y*80)
+    data["ymoves"].append(y*80)#adds coordinates to list the computer doesn't repeat spot
     data["xmoves"].append(x*80)
-    if (x*80) in data["yourShipsx"] and y*80==data["yourShipsy"][data["yourShipsx"].index((x*80))]:
+    if (x*80) in data["yourShipsx"] and y*80==data["yourShipsy"][data["yourShipsx"].index((x*80))]:#if it hits
         Sprite(RectangleAsset(80,80,LineStyle(3,Color(0x104E8B,1)),Color(0xFF0000,1)),(x*80,y*80))
         data["compHit"]+=1
-    else:
+    else:#if it misses
         Sprite(RectangleAsset(80,80,LineStyle(3,Color(0x104E8B,1)),Color(0x000000,1)),(x*80,y*80))
-    if data["compHit"]>=5:
+    if data["compHit"]>=5:#if computer wins
         Sprite(RectangleAsset(2000,2000,LineStyle(1,Color(0x000000,1)),Color(0x000000,1)))
         Sprite(TextAsset("YouLose",fill=Color(0xFFFFFF,1),style="40pt Times bold"),(490,250))
 
@@ -74,43 +67,32 @@ def computerTurn():
 #if player hasnt placed ships, place ship, if has placed ship, process user's guess if valid and detect if player won
 def mouseClick(event):
     data["click"]+=1
-    if event.x<400 and event.y<400:
+    if event.x<400 and event.y<400:#to place ships
         if data["click"]<=5:
             data["x1"]=(event.x-event.x%80)
             data["y1"]=(event.y-event.y%80)
             data["yourShipsx"].append(data["x1"])
             data["yourShipsy"].append(data["y1"])
             Sprite(RectangleAsset(80,80,LineStyle(3,Color(0x104E8B,1)),Color(0xFF3E96,1)),(data["x1"],data["y1"]))
-    elif event.x>=600 and event.y<=400 and event.y>=0 and event.x<=1000:
+    elif event.x>=600 and event.y<=400 and event.y>=0 and event.x<=1000:#on computer's side
         x=(event.x-((event.x-600)%80))
         y=(event.y-event.y%80)
-        if x-600 in data["compShipsx"] and y==data["compShipsy"][data["compShipsx"].index(x-600)]:
-            Sprite(RectangleAsset(80,80,LineStyle(3,Color(0x104E8B,1)),Color(0xFF3E96,1)),(x,y))
+        if x-600 in data["compShipsx"] and y==data["compShipsy"][data["compShipsx"].index(x-600)]:#if hit
+            Sprite(RectangleAsset(80,80,LineStyle(3,Color(0xFF0000,1)),Color(0xFF3E96,1)),(x,y))
             data["Hits"]+=1
-        else:
-            Sprite(RectangleAsset(80,80,LineStyle(3,Color(0x104E8B,1)),Color(0x00F5FF,1)),(x,y))
-        if data["Hits"]<5:
+        else:#if miss
+            Sprite(RectangleAsset(80,80,LineStyle(3,Color(0x000000,1)),Color(0x00F5FF,1)),(x,y))
+        if data["Hits"]<5:#computer's turn
             computerTurn()
-        else:
+        else:#if you win
             Sprite(RectangleAsset(2000,2000,LineStyle(3,Color(0x104E8B,1)),Color(0xFF3E96,1)))
             Sprite(TextAsset("YouWin",fill=Color(0xFFFFFF,1),style="40pt Times bold"),(490,250))
-
-
-
-
-#other color: 00F5FF
 
 if __name__ == '__main__':
     data={}
     data["click"]=0
     data["x1"]=0
     data["y1"]=0
-    data["x2"]=0
-    data["y2"]=0
-    data["x3"]=0
-    data["y3"]=0
-    data["option1"]=0
-    data["option2"]=0
     data["xmoves"]=[]
     data["ymoves"]=[]
     data["compHit"]=0
@@ -129,20 +111,3 @@ if __name__ == '__main__':
 
 App().run()
 App().listenMouseEvent("click",mouseClick)
-
-#NOT ALL COPM SHIPS IN LIST
-#LONG MY SHIPS NOT IN LIST ETS
-
-
-
-#look at matrix demo
-#To delete all graphics, you can use a for loop.
-#for item in App().spritelist[:]:
-#    item.destroy()
-#I used some constants like EMPTY = 0, MISS = 1, HIT = 2,make it easier to keep track of the numbers in the matrices.
-
-#Ideas for extensions:
-#1) Make a two player version
-#2) Allow for ships that are more than one space
-#3) Allow the user to chose the board size and/or the number of ships.
-#4) Make a cool animation when a ship is sunk.
